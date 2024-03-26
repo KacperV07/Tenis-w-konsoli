07fe861abe020442110db60007182b15b16825a7
Vicio-o <wojtekszarek17@gmail.com>
2024-03-26 23:06:52 +0100

    kort

--- gra.cpp
+++ gra.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 using namespace std;
     int main(){
-        cout<<"Hello World!";
+        cout << "        Pole przeciwnika" << "\n";
+        cout << "      ---------------------" << "\n";
+        cout << "      |   |     |     |   |" << "\n";
+        cout << "      |   |  1  |  2  |   |" << "\n";
+        cout << "      |   |     |     |   |" << "\n";
+        cout << "      | 5 |-----|-----| 6 |" << "\n";
+        cout << "      |   |     |     |   |" << "\n";
+        cout << "      |   |  3  |  4  |   |" << "\n";
+        cout << "      |   |     |     |   |" << "\n";
+        cout << "---------------------------------" << "\n";
+        cout << "      |   |     |     |   |" << "\n";
+        cout << "      |   |  1  |  2  |   |" << "\n";
+        cout << "      |   |     |     |   |" << "\n";
+        cout << "      | 5 |-----|-----| 6 |" << "\n";
+        cout << "      |   |     |     |   |" << "\n";
+        cout << "      |   |  3  |  4  |   |" << "\n";
+        cout << "      |   |     |     |   |" << "\n";
+        cout << "      ---------------------" << "\n";
+        cout << "           Twoje pole" << "\n";
+
+        
         return 0;
     }
-    //niger // zambo
