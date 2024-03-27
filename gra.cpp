b43a255fa94db9147534e897b9a7c334ef764c02
Kacper Willmann <kacper@wbeskidy.pl>
2024-03-27 20:23:29 +0100

    Funkcja Obrona i Atak

--- gra.cpp
+++ gra.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
 using namespace std;
 
-     displayGrid() {
-    cout << "        Pole przeciwnika" << "\n";
+	//
+
+
+
+
+
+    void displayGrid() {
+    cout << "        pole przeciwnika" << "\n";
     cout << "      ---------------------" << "\n";
     cout << "      |   |     |     |   |" << "\n";
     cout << "      |   |  1  |  2  |   |" << "\n";
@@ -22,37 +31,78 @@ using namespace std;
     cout << "      ---------------------" << "\n";
     cout << "           Twoje pole" << "\n";
 }
-
-    int main(){
-
-	displayGrid();
-	
-        int pole = 0;
-	while(pole <= 0 || pole >= 7){
-		cout<<"Wybierz pole jakie chcesz zaatakować (1-6)"<<endl;
-		cin>>pole;
-	}	
-
-	switch(pole){
+	void atack(int field){
+		switch(field){
 		case 1:
-		cout<<"Zaatakowałeś pole nr."<<pole<<endl;
+		cout<<"Zaatakowałeś pole nr."<<field<<endl;
 		break;	
 		case 2:
-		cout<<"Zaatakowałeś pole nr."<<pole<<endl;
+		cout<<"Zaatakowałeś pole nr."<<field<<endl;
 		break;	
 		case 3:
-		cout<<"Zaatakowałeś pole nr."<<pole<<endl;
+		cout<<"Zaatakowałeś pole nr."<<field<<endl;
 		break;	
 		case 4:
-		cout<<"Zaatakowałeś pole nr."<<pole<<endl;
+		cout<<"Zaatakowałeś pole nr."<<field<<endl;
 		break;	
 		case 5:
-		cout<<"Zaatakowałeś pole nr."<<pole<<endl;
+		cout<<"Zaatakowałeś pole nr."<<field<<endl;
 		break;	
 		case 6:
-		cout<<"Zaatakowałeś pole nr."<<pole<<endl;
+		cout<<"Zaatakowałeś pole nr."<<field<<endl;
 		break;	
 
 	}
-        return 0;
+
+	}
+
+	void defend(int field){
+		switch(field){
+		case 1:
+		cout<<"Bronisz pole nr."<<field<<endl;
+		break;	
+		case 2:
+		cout<<"Bronisz pole nr."<<field<<endl;
+		break;	
+		case 3:
+		cout<<"Bronisz pole nr."<<field<<endl;
+		break;	
+		case 4:
+		cout<<"Bronisz pole nr."<<field<<endl;
+		break;	
+		case 5:
+		cout<<"Bronisz pole nr."<<field<<endl;
+		break;	
+		case 6:
+		cout<<"Bronisz pole nr."<<field<<endl;
+		break;	
+
+	}
+
+	}
+
+    int main(){
+		// NA RAZIE TYLKO GRA Z BOTEM!
+	displayGrid();
+	// ATACK
+        int field = 0;
+	while(field <= 0 || field >= 7){
+		cout<<"Wybierz pole jakie chcesz zaatakować (1-6) ";
+		cin>>field;
+		cout<<endl;
+	}	
+	atack(field);
+        
+   
+   // DEFEND
+    field = 0;
+	displayGrid();
+	while(field <= 0 || field >= 7){
+		cout<<"Wybierz pole jakie chcesz obronić (1-6) ";
+		cin>>field;
+		cout<<endl;
+	}
+	defend(field);	
+	return 0;
     }
+	
