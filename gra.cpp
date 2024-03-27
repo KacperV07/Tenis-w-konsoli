c4e8947bc91b24e88ce89b8f403f26ed437dbf77
KacperV07 <99750904+KacperV07@users.noreply.github.com>
2024-03-27 09:11:42 +0100

    Update gra.cpp
    Switch

--- gra.cpp
+++ gra.cpp
@@ -21,6 +21,32 @@ using namespace std;
         cout << "      ---------------------" << "\n";
         cout << "           Twoje pole" << "\n";
 
-        
+        int pole = 0;
+	while(pole <= 0 || pole >= 7){
+		cout<<"Wybierz pole jakie chcesz zaatakować (1-6)"<<endl;
+		cin>>pole;
+	}	
+
+	switch(pole){
+		case 1:
+		cout<<"Zaatakowałeś pole nr."<<pole<<endl;
+		break;	
+		case 2:
+		cout<<"Zaatakowałeś pole nr."<<pole<<endl;
+		break;	
+		case 3:
+		cout<<"Zaatakowałeś pole nr."<<pole<<endl;
+		break;	
+		case 4:
+		cout<<"Zaatakowałeś pole nr."<<pole<<endl;
+		break;	
+		case 5:
+		cout<<"Zaatakowałeś pole nr."<<pole<<endl;
+		break;	
+		case 6:
+		cout<<"Zaatakowałeś pole nr."<<pole<<endl;
+		break;	
+
+	}
         return 0;
     }
